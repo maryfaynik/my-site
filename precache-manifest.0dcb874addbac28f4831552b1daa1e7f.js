9351ce21f05b11dac1c632860b0119f2d8ef31bd
maryfarner18 <maryfarner@gmail.com>
2020-01-04 23:29:40 -0500

    Updates

---
+++ precache-manifest.0dcb874addbac28f4831552b1daa1e7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7835c7373a8b647e3f73c3e75dd75fcd",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "78061d77d4b87fb842bd",
+    "url": "/my-site/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e1cc62859b4abfa45eec",
+    "url": "/my-site/static/js/2.b1258573.chunk.js"
+  },
+  {
+    "revision": "78061d77d4b87fb842bd",
+    "url": "/my-site/static/js/main.10d9dd4a.chunk.js"
+  },
+  {
+    "revision": "cce256b4ab75633c3bf6",
+    "url": "/my-site/static/js/runtime-main.c23d458d.js"
+  }
+]);
