eb0da35b0ea4f84305b4c9195137b42046e2e078
maryfarner18 <maryfarner@gmail.com>
2020-06-15 09:46:59 -0400

    Update resume

--- src/components/resume/resumeData.js
+++ src/components/resume/resumeData.js
@@ -4,6 +4,14 @@ let resumeData = {
     "work_experience": [
       {
         "id": 1,
+        "company": "Milo",
+        "position": "Fullstack Software Developer",
+        "start": "2020",
+        "end": "Present",
+        "description": "Developing software products to make veterinary healthcare workers happy. "
+      },
+      {
+        "id": 2,
         "company": "Chelsea Piers",
         "position": "Project Manager, Corporate Planning",
         "start": "2019",
@@ -11,7 +19,7 @@ let resumeData = {
         "description": "Managed an enterprise-wide multi-million dollar technology review and redesign including assessment of the user experience and web presence, financial reporting and back office software tools, and operational software used to run the businesses."
       },
       {
-        "id": 2,
+        "id": 3,
         "company": "Chelsea Piers Fitness",
         "position": "Regional Group Fitness Director",
         "start": "2018",
@@ -19,7 +27,7 @@ let resumeData = {
         "description": "Managed all operations and logistical planning including scheduling, budgeting, and programming for Chelsea Piers’ Group Fitness program across both club locations in Manhattan and Brooklyn, New York. Directly managed a team of 70+ instructors and three full-time employees with oversight of hiring and payroll processes. Assisted in the development and operations of Chelsea Piers’ expansion into Brooklyn as a part of the organization’s original management team." 
       },
       {
-        "id": 3,
+        "id": 4,
         "company": "Bank of America Merrill Lynch",
         "position": "Trader - Analyst",
         "start": "2017",
@@ -27,7 +35,7 @@ let resumeData = {
         "description": "Funded daily FX positions and supported senior  traders in market-making, pricing, reporting, and risk management. Sector focus on FX and cross-currency basis swaps, interest rate swaps, and futures. Passed all requisite FINRA Series Licensing Exams (Series 7 and Series 63 certified)."
       },
       {
-        "id": 4,
+        "id": 5,
         "company": "United States Air Force",
         "position": "Associate Computer Scientist",
         "start": "2016",
@@ -52,7 +60,7 @@ let resumeData = {
       }
     ],
     "skills": {
-      "tech": [ "JavaScript", "React", "Ruby", "Rails", "SQLite", "PostgreSQL", "C", "Java", "Python", "C++", "CSS", "HTML", "SQL", "Heroku", "AWS", "Github", "MS Office"],
+      "tech": [ "JavaScript", "React", "Ruby", "Rails","GraphQL", "SQLite", "PostgreSQL", "C", "Java", "Python", "C++", "CSS", "HTML", "MJML", "SQL", "Heroku", "AWS", "Github"],
       "interests": ["Fitness", "Running", "Rowing", "Marathon", "Triathlon", "Golf", "Cooking", "Knitting", "Crossfit", "Spinning"]
     }
     }
