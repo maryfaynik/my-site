9ed0f0670289495cab6c7d34e1d5932e8839115e
Mary Farner <maryfarner@gmail.com>
2023-05-18 15:53:47 -0400

    UPDate for Bond

--- src/components/resume/resumeData.js
+++ src/components/resume/resumeData.js
@@ -4,14 +4,22 @@ let resumeData = {
     "work_experience": [
       {
         "id": 1,
-        "company": "Milo",
+        "company": "Bond Vet",
         "position": "Fullstack Software Developer",
-        "start": "February, 2020",
+        "start": "April, 2022",
         "end": "Present",
-        "description": "Developing software products to make veterinary healthcare workers happy. Creating full-stack solutions for a quickly growing web app, with a focus on API integrations with third parties. Collaborate closely with the product team and end users to define, develop, execute, and troubleshoot features."
+        "description": "Contributes to the growth of both internal and external client-facing web applications. Applies a slight back end / data focus to support integrations and features supporting operational and financial performance and growth."
       },
       {
         "id": 2,
+        "company": "Milo",
+        "position": "Fullstack Software Developer",
+        "start": "February, 2020",
+        "end": "March 2022",
+        "description": "Developed software products to make veterinary healthcare workers happy. Created full-stack solutions for a quickly growing web app, with a focus on API integrations with third parties. Collaborated closely with the product team and end users to define, develop, execute, and troubleshoot features."
+      },
+      {
+        "id": 3,
         "company": "Chelsea Piers",
         "position": "Project Manager, Corporate Planning",
         "start": "2019",
@@ -19,7 +27,7 @@ let resumeData = {
         "description": "Managed an enterprise-wide multi-million dollar technology review and redesign including assessment of the user experience and web presence, financial reporting and back office software tools, and operational software used to run the businesses."
       },
       {
-        "id": 3,
+        "id": 4,
         "company": "Chelsea Piers Fitness",
         "position": "Regional Group Fitness Director",
         "start": "2018",
@@ -27,7 +35,7 @@ let resumeData = {
         "description": "Managed all operations and logistical planning including scheduling, budgeting, and programming for Chelsea Piers’ Group Fitness program across both club locations in Manhattan and Brooklyn, New York. Directly managed a team of 70+ instructors and three full-time employees with oversight of hiring and payroll processes. Assisted in the development and operations of Chelsea Piers’ expansion into Brooklyn as a part of the organization’s original management team." 
       },
       {
-        "id": 4,
+        "id": 5,
         "company": "Bank of America Merrill Lynch",
         "position": "Trader - Analyst",
         "start": "2017",
@@ -35,7 +43,7 @@ let resumeData = {
         "description": "Funded daily FX positions and supported senior  traders in market-making, pricing, reporting, and risk management. Sector focus on FX and cross-currency basis swaps, interest rate swaps, and futures. Passed all requisite FINRA Series Licensing Exams (Series 7 and Series 63 certified)."
       },
       {
-        "id": 5,
+        "id": 6,
         "company": "United States Air Force",
         "position": "Associate Computer Scientist",
         "start": "2016",
@@ -60,7 +68,7 @@ let resumeData = {
       }
     ],
     "skills": {
-      "tech": [ "React", "Ruby", "Rails", "GraphQL", "JavaScript", "Typescript", "PostgreSQL", "Heroku", "AWS", "S3", "Redis", "Sidekiq", "Clockwork", "Github",  "SQL", "HTTP", "SOAP", "XML", "JSON", "C", "Java", "Python", "C++", "SQLite",  "CSS", "HTML", "MJML"],
+      "tech": [ "React", "Ruby", "Rails", "NodeJS", "GraphQL", "JavaScript", "Typescript", "MongoDb", "PostgreSQL", "Heroku", "AWS", "S3", "Google Cloud", "Redis", "Github",  "SQL", "HTTP", "SOAP", "XML", "JSON", "C", "Java", "Python", "C++", "SQLite",  "CSS", "HTML", "MJML"],
       "interests": ["Fitness", "Running", "Cycling", "Rowing", "Marathon", "Triathlon", "Golf", "Cooking", "Knitting"]
     }
     }
