677a9632bb192468dc2c617a8f97c1287ed26c60
Mary Farner <maryfarner@gmail.com>
2023-05-18 15:52:58 -0400

    Updates

---
+++ precache-manifest.2e6d3c70c3b33df4ac6a41e3ba8bb1a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e994836f7af447aac54",
+    "url": "/static/css/main.b3644d0c.chunk.css"
+  },
+  {
+    "revision": "4e994836f7af447aac54",
+    "url": "/static/js/main.4e994836.chunk.js"
+  },
+  {
+    "revision": "5d4e9a67008e4c1869e5",
+    "url": "/static/js/1.5d4e9a67.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "33b97d468ae0e829dd103b752f022cfc",
+    "url": "/index.html"
+  }
+];
