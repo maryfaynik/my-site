d9ec3d6c9c1bbb6a60d55c6463ad043ed5882ba6
Mary Faynik <maryfarner@gmail.com>
2025-01-16 10:38:50 -0500

    Fix gh link

--- src/components/header/headerData.js
+++ src/components/header/headerData.js
@@ -2,7 +2,7 @@ export const info = {
     "name": "Mary Faynik",
 
     "links": {
-        "github": "https://github.com/maryfarner18",
+        "github": "https://github.com/maryfaynik",
         "linkedin": "https://www.linkedin.com/in/mary-faynik/",
     }
 }
